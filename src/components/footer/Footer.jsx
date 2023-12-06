19405df7d7e20246fa0eacaa0a414435e9b9e9f6
Krishan17212 <73638230+Krishan17212@users.noreply.github.com>
2023-12-06 17:35:03 +0530

    footer change

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,46 +4,62 @@ import {
   FaTwitter,
   FaLinkedin,
 } from "react-icons/fa";
-
+import { useNavigate, useLocation } from "react-router-dom";
 import ContentWrapper from "../contentWrapper/ContentWrapper";
-import './style.scss'
+import logo from "../../assets/filmflow.png";
+import "./style.scss";
 
 const Footer = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const scrollUp = () => {
+    navigate("/")
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // Optional: Adds a smooth scrolling animation
+      });
+  }
   return (
     <footer className="footer">
-            <ContentWrapper>
-                <ul className="menuItems">
+      <ContentWrapper>
+        <div className="footer_logo">
+          <div className="logo">
+            <img  src={logo} alt="FilmFlow" onClick={scrollUp} />
+          </div>
+        </div>
+        {/* <ul className="menuItems">
                     <li className="menuItem">Terms Of Use</li>
                     <li className="menuItem">Privacy-Policy</li>
                     <li className="menuItem">About</li>
                     <li className="menuItem">Blog</li>
                     <li className="menuItem">FAQ</li>
-                </ul>
-                <div className="infoText">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur.
-                </div>
-                <div className="socialIcons">
-                    <span className="icon">
-                        <FaFacebookF />
-                    </span>
-                    <span className="icon">
-                        <FaInstagram />
-                    </span>
-                    <span className="icon">
-                        <FaTwitter />
-                    </span>
-                    <span className="icon">
-                        <FaLinkedin />
-                    </span>
-                </div>
-            </ContentWrapper>
-        </footer>
-  )
-}
+                </ul> */}
+        <div className="infoText">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur.
+        </div>
+        <div className="socialIcons">
+          <span className="icon">
+            <FaFacebookF />
+          </span>
+          <span className="icon">
+            <FaInstagram />
+          </span>
+          <span className="icon">
+            <FaTwitter />
+          </span>
+          <span className="icon">
+            <FaLinkedin />
+          </span>
+        </div>
+      </ContentWrapper>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -81,7 +81,7 @@ const Header = () => {
     <header className={`header ${mobileMenu ? "mobileView" : ""} ${show}`}>
       <ContentWrapper>
         <div className="logo">
-          <img src={logo} alt="ProfesStudio" onClick={() => navigate("/")} />
+          <img src={logo} alt="FilmFlow" onClick={() => navigate("/")} />
         </div>
         <ul className="menuItems">
           <li className="menuItem" onClick={() => navigationHandler('movie')}>Movies</li>
